377e1d8e913c80995fd6a15a465f103c413607ca
Ahmed Atta <ahmedelsaikaly@gmail.com>
2020-11-14 14:37:38 +0200

    start backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import Header from "./components/Header"
+import {Container} from "react-bootstrap"
+import Footer from "./components/Footer"
+import  HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App=()=> {
   return (
-    <>
-    <h1>Hello from gaza</h1>
-    </>
+    <Router>
+    <Header />
+    <main className="py-3" >
+      <Container>
+        <Route  path = {"/"} component={HomeScreen} exact/>
+        <Route path={"/product/:id"} component={ProductScreen} />
+      </Container>
+    </main>
+    <Footer />
+    </Router >
   )
 
 }
